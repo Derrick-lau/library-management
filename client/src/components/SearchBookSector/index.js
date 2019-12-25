b16bd419ed8b80dd62b2acc25dfd884bd66358ae
Derrick Lau <derrcikalau@gmail.com>
2019-12-25 01:04:30 +0000

    fixed naming

--- client/src/components/SearchBookSector/index.js
+++ client/src/components/SearchBookSector/index.js
@@ -5,17 +5,17 @@ import Input from '../Styled-Input';
 import {MapBooks} from '../mapTable'
 import Search from '../SearchFetch';
 
-const SearchBook = ({history}) => {
+const SearchBook = () => {
 
   //books that got from server
   const [fetchedbooks, setFetchedBooks] = useState([{id: null, title: null, isbn: null, authors: null}]);
 
   //books that will get from server
-  const [fetchBook, setfetchBook] = useState({title: null, authors: null});
+  const [BooktoRequest, setBooktoRequest] = useState({title: null, authors: null});
 
   // SearchBook with Search Function
   const SearchBook = () => {
-    const searchBook = Search('http://localhost:3000/books/search', {title: fetchBook.title, authors: fetchBook.authors}, setFetchedBooks, fetchBook.title )
+    const searchBook = Search('http://localhost:3000/books/search', {title: BooktoRequest.title, authors: BooktoRequest.authors}, setFetchedBooks, BooktoRequest.title )
   }
   // add books that got from server to the table
   const mappedBooks = fetchedbooks.map(({...args}, i) => <MapBooks {...args} key={i}/>);
@@ -23,7 +23,7 @@ const SearchBook = ({history}) => {
   return (
     <>
     <div className ="serachFileld">
-      <Input name='title/author' type='text' placeholder="Title or Author" required onChange={e => setfetchBook({title: e.target.value, authors: e.target.value})}/>
+      <Input name='title/author' type='text' placeholder="Title or Author" required onChange={e => setBooktoRequest({title: e.target.value, authors: e.target.value})}/>
       <Button variant="secondary"  onClick={SearchBook} size="sm" required>
         Search
       </Button>
--- client/src/components/SearchFetch/index.js
+++ client/src/components/SearchFetch/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const Search = (url, DataToServer, StateSet, name) =>{
+const Search = (url, DataRequested, StateSet, name) =>{
     const SearchRequest = async(err) => {
       try {  
         const res = await axios({
         url: url,
         method: 'post',
-        data: DataToServer
+        data: DataRequested
       });
         {
           res.status===200 && res.data.length>=1 && name !=='' ?
