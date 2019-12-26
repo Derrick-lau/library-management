4de9688eb8d8bd8fd79d43e3c71f29b421c7cc7c
Derrick Lau <derrcikalau@gmail.com>
2019-12-26 01:12:25 +0000

    fixed naming

--- client/src/components/SearchBookSector/index.js
+++ client/src/components/SearchBookSector/index.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react';
 import './index.scss';
 import { Table, Button,} from 'react-bootstrap';
 import Input from '../Styled-Input';
-import {MapBooks} from '../mapTable'
-import Search from '../SearchFetch';
+import SearchRequest from '../SearchRequest';
 
 const SearchBook = () => {
 
@@ -13,17 +12,22 @@ const SearchBook = () => {
   //books that will get from server
   const [BooktoRequest, setBooktoRequest] = useState({title: null, authors: null});
 
-  // SearchBook with Search Function
-  const SearchBook = () => {
-    const searchBook = Search('http://localhost:3000/books/search', {title: BooktoRequest.title, authors: BooktoRequest.authors}, setFetchedBooks, BooktoRequest.title )
-  }
-  // add books that got from server to the table
-  const mappedBooks = fetchedbooks.map(({...args}, i) => <MapBooks {...args} key={i}/>);
+  const HandleSearch = e => setBooktoRequest({title: e.target.value, authors: e.target.value})
 
+  // Fetch Books from server
+  const SearchBook = () => {
+    const searchBook = SearchRequest('http://localhost:3000/books/search', {title: BooktoRequest.title, authors: BooktoRequest.authors}, setFetchedBooks, BooktoRequest.title )
+  }
+
+  const mappedBooks = fetchedbooks.map(({id, title, isbn, authors},i) =>     
+    <tr key={i}> 
+      <th scope="row">{id}</th><td>{title}</td><td>{isbn}</td><td>{authors}</td>
+    </tr>);
+                                                  
   return (
     <>
     <div className ="serachFileld">
-      <Input name='title/author' type='text' placeholder="Title or Author" required onChange={e => setBooktoRequest({title: e.target.value, authors: e.target.value})}/>
+      <Input name='title/author' type='text' placeholder="Title or Author" required onChange={HandleSearch}/>
       <Button variant="secondary"  onClick={SearchBook} size="sm" required>
         Search
       </Button>
--- client/src/components/SearchRequest/index.js
+++ client/src/components/SearchRequest/index.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const Search = (url, DataRequested, StateSet, name) =>{
-    const SearchRequest = async(err) => {
+const SearchRequest = (url, data, stateSet, name) =>{
+    const searchRequest = async(err) => {
       try {  
         const res = await axios({
         url: url,
         method: 'post',
-        data: DataRequested
+        data: data
       });
         {
           res.status===200 && res.data.length>=1 && name !=='' ?
-          StateSet(res.data) : StateSet([{id:'Not found'}]);
+          stateSet(res.data) : stateSet([{id:'Not found'}]);
         }
       } catch {console.log(err)};
     }
-    SearchRequest();
+    searchRequest();
   }
 
 
-export default Search;
+export default SearchRequest;
